7e5d1b0e7bb2670007fe4e9595a90cefeb975d5b
Don Romaniello <don.romaniello@gmail.com>
2022-05-26 18:29:33 -0400

    First commit.

--- src/App.js
+++ src/App.js
@@ -9,16 +9,17 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
+
 import { Logo } from './Logo';
+import Header from './Header'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
+            <Header />
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
               Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
